126ac6d2f56cbf977e90eb7c9da6851f38e46e66
Arte-Wright <100436567+Arte-Wright@users.noreply.github.com>
2024-06-08 10:45:11 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# Internship Portal
+
+### Technologies Used
+
+- HTML5
+- CSS
+- PHP
+- MYSQL
+- Ajax
+- Bootstrap
+- JS
+
+[![forthebadge](https://forthebadge.com/images/badges/built-with-love.svg)](https://github.com/Arte-Wright/InternshipBridge)
+
+
+
+
+
+
+
+
+
---
+++ index.php
@@ -0,0 +1,305 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Internship Bridge</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+
+  <link href="img/job.png" rel="icon">
+  <link href="img/job.png" rel="apple-touch-icon">
+
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
+
+
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+
+  <link href="css/style.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+
+  <header id="header">
+    <div class="container-fluid">
+
+      <div id="logo" class="pull-left">
+        <h1><a href="#intro" class="scrollto">Internship Bridge</a></h1>
+
+      </div>
+
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <li class="menu-active"><a href="#intro">Home</a></li>
+          <li><a href="#about">About Us</a></li>
+          <li><a href="#services">Services</a></li>
+          
+          <li class="menu-has-`children"><a href="studentlogin.php">Login</a>
+            <ul>
+              <li><a href="emplogin.php">Company</a></li>
+              <li><a href="studentlogin.php">Student</a></li>
+            </ul>
+          </li>
+          <li class="menu-has-`children"><a href="studentreg.php">Registration</a>
+            <ul>
+              <li><a href="empreg.php">Company</a></li>
+              <li><a href="studentreg.php">Student</a></li>
+            </ul>
+          </li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
+  <section id="intro">
+    <div class="intro-container">
+      <div id="introCarousel" class="carousel  slide carousel-fade" data-ride="carousel">
+
+        <ol class="carousel-indicators"></ol>
+
+        <div class="carousel-inner" role="listbox">
+
+          <div class="carousel-item active">
+            <div class="carousel-background"><img src="img/1.jpg" alt=""></div>
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h5>"Interns are like not-cut diamonds; They have flaws, some of them are totally worthless, but once you cut them well, they can become something great one day, And that’s what I’m doing, I’m cutting diamonds…"</h5>
+                <a href="jobs.php" class="btn-get-started scrollto">Find Your Internship</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="carousel-item">
+            <div class="carousel-background"><img src="img/2.jpg" alt=""></div>
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h5>Internships are very important part of our education program, first and foremost. In setting up professional internships, not only will we be able to offer students a chance to work with industry professionals, but the students also will be able to take the theory they learn in the classroom and put it into practice here, in whichever activity they’re focused.</h5>
+                <a href="jobs.php" class="btn-get-started scrollto">Find Your Internship</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="carousel-item">
+            <div class="carousel-background"><img src="img/3.jpg" alt=""></div>
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h5>“The only way to do great work is to love what you do. If you haven’t found it yet, keep looking. Don’t settle. As with all matters of the heart, you’ll know when you find it.” </h5>
+                <a href="jobs.php" class="btn-get-started scrollto">Find Your Internship</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="carousel-item">
+            <div class="carousel-background"><img src="img/4.jpg" alt=""></div>
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h5>“My advice for young people is, study what you love and intern in what you want to do. And I think it’s okay to pivot as many times as you need to."</h5>
+                <a href="jobs.php" class="btn-get-started scrollto">Find Your Internship</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="carousel-item">
+            <div class="carousel-background"><img src="img/5.jpg" alt=""></div>
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h5>"The goal is not to be successful, the goal is to be valuable. Once you're valuable, instead of chasing success, it will attract itself to you."</h5>
+                <a href="jobs.php" class="btn-get-started scrollto">Find Your Internship</a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <a class="carousel-control-prev" href="#introCarousel" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon ion-chevron-left" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+
+        <a class="carousel-control-next" href="#introCarousel" role="button" data-slide="next">
+          <span class="carousel-control-next-icon ion-chevron-right" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+
+      </div>
+    </div>
+  </section>
+
+
+
+
+
+  <main id="main">
+
+    <section id="about">
+      <div class="container">
+
+        <header class="section-header">
+          <h3>About Us</h3>
+          <p> This website is for students as well as for companies. </p>
+        </header>
+      </div>
+    </section>
+
+
+    <section id="services">
+      <div class="container">
+
+        <header class="section-header  wow fadeInUp">
+          <h3>Services</h3>
+          <p>This Website will provide you a lot of services. Those are below.</p>
+          <table align="center">
+            <tr>
+              <td align="center"> 1. Providing efficiency. </td>
+            </tr>
+            <tr>
+              <td align="center"> 2. Providing interactive user interface.</td>
+            </tr>
+            <tr>
+              <td align="center"> 3. Companies can post for an internship.</td>
+            </tr>
+            <tr>
+              <td align="center"> 4. Student can apply for an internship. </td>
+            </tr>
+
+          </table>
+        </header>
+      </div>
+    </section>
+
+
+   
+    <section id="contact" class="section-bg wow fadeInUp">
+      <div class="container">
+
+        <div class="section-header">
+          <h3>Contact Us</h3>
+          <p> For any kind of information or any type of help you need, then Contact Us below.</p>
+        </div>
+
+        <div class="row contact-info">
+
+          <div class="col-md-4">
+            <div class="contact-address">
+              <i class="ion-ios-location-outline"></i>
+              <h3>Address</h3>
+              <address>  Thapar Institute of Engineering & Technology, 
+              Patiala, Pin-147004,
+              INDIA. </address>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="contact-phone">
+              <i class="ion-ios-telephone-outline"></i>
+              <h3>Phone Number</h3>
+              <p><a href="tel:+91 8622088880">+91 8622088880</a></p>
+              <p><a href="tel:+91 6280672830">+91 6280672830</a></p>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="contact-email">
+              <i class="ion-ios-email-outline"></i>
+              <h3>Email</h3>
+              <p><a href="https://www.gmail.com">swraich_be21@thapar.edu</a></p>
+              <p><a href="https://www.gmail.com">aayush_be21@thapar.edu</a></p>
+            
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+  </main>
+
+
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 footer-info">
+            <h3>Internship Bridge</h3>
+            <p>This is a Website where company can post an internship and student can apply for that. This Website is developed by students of Thaper Institute of Engineering & Technology(TIET).</p>
+
+          </div>
+          &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+          <div class="col-lg-3 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="ion-ios-arrow-right"></i> <a href="#intro">Home</a></li>
+              <li><i class="ion-ios-arrow-right"></i> <a href="#about">About us</a></li>
+              <li><i class="ion-ios-arrow-right"></i> <a href="#services">Services</a></li>
+              <li><i class="ion-ios-arrow-right"></i> <a href="#">Terms of service</a></li>
+              <li><i class="ion-ios-arrow-right"></i> <a href="#">Privacy policy</a></li>
+            </ul>
+          </div>
+
+          &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h4>Contact Us</h4>
+            <p>
+              Thapar Institute of Engineering & Technology, <br>
+              Patiala, Pin-147004,<br>
+              INDIA. <br>
+              <strong>Phone:</strong> +91-175-2393021<br>
+              <strong>Fax:</strong> +91-175-2364498<br>
+              <strong>Email:</strong> registrar@thapar.edu<br>
+            </p>
+
+         
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong>Internship Bridge</strong>. All Rights Reserved | The template is made by <span>Ayush & Sunderdeep</span>
+      </div>
+    </div>
+  </footer>
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/superfish/hoverIntent.js"></script>
+  <script src="lib/superfish/superfish.min.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/waypoints/waypoints.min.js"></script>
+  <script src="lib/counterup/counterup.min.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="lib/isotope/isotope.pkgd.min.js"></script>
+  <script src="lib/lightbox/js/lightbox.min.js"></script>
+  <script src="lib/touchSwipe/jquery.touchSwipe.min.js"></script>
+  <script src="contactform/contactform.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="js/main.js"></script>
+
+</body>
+
+</html>
